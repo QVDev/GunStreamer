fffb29d44b721d1cdcb63dc06bd26950735f5e73
Martti Malmi <sirius@iki.fi>
2019-09-20 10:56:07 +0300

    gunViewer -> this

--- js/GunViewer.js
+++ js/GunViewer.js
@@ -14,11 +14,11 @@ class GunViewer {
         if (this.video !== null) {
             this.mediaBuffer.load();
             this.video.src = window.URL.createObjectURL(this.mediaSource);
-            this.mediaSource.addEventListener('sourceopen', function () {
-                this.sourceBuffer = this.addSourceBuffer(gunViewer.mimeType);
+            this.mediaSource.addEventListener('sourceopen', () => {
+                this.sourceBuffer = this.addSourceBuffer(this.mimeType);
                 this.sourceBuffer.mode = 'sequence';
                 // Get video segments and append them to sourceBuffer.
-                gunViewer.debugLog("Source is open and ready to append to sourcebuffer");
+                this.debugLog("Source is open and ready to append to sourcebuffer");
             });
         } else {
             this.debugLog("There is no video present with this ID");
@@ -27,40 +27,40 @@ class GunViewer {
 
     showDelay() {
         let currentTime = new Date().getTime();
-        if (gunViewer.lastTime != 0) {
-            var delay = (currentTime - gunViewer.lastTime) / 1000;
-            gunViewer.debugLog("current delay::" + delay);
-            gunViewer.mediaBuffer.addDelay(delay);
-            gunViewer.debugLog("Average Media delay::" + gunViewer.mediaBuffer.getAverageDelay());
+        if (this.lastTime != 0) {
+            var delay = (currentTime - this.lastTime) / 1000;
+            this.debugLog("current delay::" + delay);
+            this.mediaBuffer.addDelay(delay);
+            this.debugLog("Average Media delay::" + this.mediaBuffer.getAverageDelay());
         }
-        gunViewer.lastTime = currentTime;
+        this.lastTime = currentTime;
     }
 
     onStreamerData(userData) {
-        gunViewer.showDelay()
-        gunViewer.debugLog(userData);
-        if (gunViewer.video.readyState != 0) {
-            gunViewer.debugLog("regular data")
-            gunViewer.appendBuffer(userData.data);
+        this.showDelay()
+        this.debugLog(userData);
+        if (this.video.readyState != 0) {
+            this.debugLog("regular data")
+            this.appendBuffer(userData.data);
         } else {
-            gunViewer.debugLog("initial data")
-            gunViewer.appendBuffer(userData.initial);
+            this.debugLog("initial data")
+            this.appendBuffer(userData.initial);
         }
 
-        if (gunViewer.video.readyState >= 2 && gunViewer.video.paused) {
-            gunViewer.video.play();
+        if (this.video.readyState >= 2 && this.video.paused) {
+            this.video.play();
         }
     }
 
     appendBuffer(base64Data) {
         let byteCharacters = atob(base64Data);
-        let byteArray = gunViewer.str2ab(byteCharacters);
+        let byteArray = this.str2ab(byteCharacters);
 
-        if (!gunViewer.mediaSource.sourceBuffer.updating) {
-            gunViewer.debugLog("append to buffer")
-            gunViewer.mediaSource.sourceBuffer.appendBuffer(byteArray);
+        if (!this.mediaSource.sourceBuffer.updating) {
+            this.debugLog("append to buffer")
+            this.mediaSource.sourceBuffer.appendBuffer(byteArray);
         } else {
-            gunViewer.debugLog("BUFFER STILL BUSY")
+            this.debugLog("BUFFER STILL BUSY")
         }
 
         byteCharacters = null;
